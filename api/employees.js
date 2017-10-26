3be13fd00b41817be5ff8119ec2c1aabe4a6f836
Robert Tuck <robtuck@gmail.com>
2017-10-26 00:03:57 -0400

    Implemented GET and POST for /api/employees
    .  Implemented GET for /api/employees/:employeeId.  added param callback for employeesRouter to check for valid employeeId.

--- api/employees.js
+++ api/employees.js
@@ -9,3 +9,57 @@ const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite'
 
 const timesheetsRouter = require('./timesheets');
 employeesRouter.use('/:employeeId/timesheets', timesheetsRouter);
+
+employeesRouter.param('employeeId', (req, res, next, employeeId) => {
+  db.get('SELECT * FROM employee WHERE id=$id', { $id: employeeId }, (error, employee) => {
+    if (employee) {
+      req.employee = employee;
+      next();
+    } else {
+      res.status(404).send();
+    }
+  });
+});
+
+employeesRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Employee WHERE is_current_employee = 1', (error, employees) => {
+    if (error) {
+      next(error);
+    } else {
+      res.status(200).json({employees: employees});
+    }
+  });
+});
+
+employeesRouter.post('/', (req, res, next) => {
+  const name = req.body.employee.name;
+  const position = req.body.employee.position;
+  const wage = req.body.employee.wage;
+  const isCurrentEmployee = req.body.employee.isCurrentEmployee === 0 ? 0 : 1;
+  if (!name || !position || !wage ) {
+    res.status(400).send();
+  } else {
+    const employeeSql = 'INSERT INTO Employee (name, position, wage, is_current_employee) ' +
+                        'VALUES ($name, $position, $wage, $isCurrentEmployee)';
+    const employeeVals = {
+      $name: name,
+      $position: position,
+      $wage: wage,
+      $isCurrentEmployee: isCurrentEmployee
+    };
+    db.run(employeeSql, employeeVals, function(error) {
+      if (error) {
+        next(error);
+      } else {
+        db.get(`SELECT * FROM Employee WHERE Employee.id = ${this.lastID}`,
+        (error, employee) => {
+          res.status(201).json({employee: employee});
+        });
+      }
+    });
+  }
+});
+
+employeesRouter.get('/:employeeId', (req, res, next) => {
+  res.status(200).json({employee: req.employee});
+});
