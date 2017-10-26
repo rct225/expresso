a8ef83e41cd8b6d8b26e7367a59843a20d731c8d
Robert Tuck <robtuck@gmail.com>
2017-10-26 09:11:15 -0400

    Implemented GET /api/employees/:employeeId/timesheets in timesheets.js

--- api/timesheets.js
+++ api/timesheets.js
@@ -6,3 +6,15 @@ module.exports = timesheetsRouter;
 
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+timesheetsRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Timesheet WHERE employee_id = $employeeId',
+    { $employeeId: req.employee.id},
+    (error, timesheets) => {
+      if (error) {
+        next(error);
+      } else {
+        res.status(200).json({timesheets: timesheets});
+      }
+    });
+});
