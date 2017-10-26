a10d743bc3e647a169172e126cceebb855993e0a
Robert Tuck <robtuck@gmail.com>
2017-10-25 23:27:12 -0400

    fixed a few typos from cutting and pasting

--- api/menus.js
+++ api/menus.js
@@ -7,5 +7,5 @@ module.exports = menusRouter;
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
-const menusItemRouter = require('./menuItems');
-employeesRouter.use('/:menuId/menuItems', menuItemsRouter);
+const menuItemsRouter = require('./menuItems');
+menusRouter.use('/:menuId/menuItems', menuItemsRouter);
