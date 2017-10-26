f5c4d2a1437b0a290a17d99a1b7c10ddd078fd07
Robert Tuck <robtuck@gmail.com>
2017-10-26 09:45:31 -0400

    Implemented PUT /api/employees/:employeeId/timesheets/:timesheetId in timesheets.js.  Added param callback to validate timesheetId.

--- api/timesheets.js
+++ api/timesheets.js
@@ -7,6 +7,18 @@ module.exports = timesheetsRouter;
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+
+timesheetsRouter.param('timesheetId', (req, res, next, timesheetId) => {
+  db.get('SELECT * FROM employee WHERE id=$id', { $id: timesheetId }, (error, timesheet) => {
+    if (timesheet) {
+      req.timesheet = timesheet;
+      next();
+    } else {
+      res.status(404).send();
+    }
+  });
+});
+
 timesheetsRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Timesheet WHERE employee_id = $employeeId',
     { $employeeId: req.employee.id},
@@ -45,6 +57,39 @@ timesheetsRouter.post('/', (req, res, next) => {
       db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = ${this.lastID}`,
                (error, timesheet) => {
                  res.status(201).json({timesheet: timesheet});
+               });
+    }
+  });
+});
+
+timesheetsRouter.put('/:timesheetId', (req, res, next) => {
+  const hours = req.body.timesheet.hours;
+  const rate = req.body.timesheet.rate;
+  const date = req.body.timesheet.date;
+  const employeeId = req.employee.id;
+
+  const timesheetSql = 'UPDATE Timesheet SET hours = $hours, rate = $rate, ' +
+                       'date = $date, employee_id = $employeeId ' +
+                       'WHERE id = $timesheetId';
+  const timesheetVals = {
+    $hours: hours,
+    $rate: rate,
+    $date: date,
+    $employeeId: employeeId,
+    $timesheetId: req.timesheet.id
+  };
+
+  if (!hours || !rate || !date || !employeeId) {
+    return res.status(400).send();
+  }
+
+  db.run(timesheetSql, timesheetVals, function(error) {
+    if (error) {
+      next(error);
+    } else {
+      db.get(`SELECT * FROM Timesheet WHERE Timesheet.id = ${req.timesheet.id}`,
+               (error, timesheet) => {
+                 res.status(200).json({timesheet: timesheet});
                });
     }
   });
