b49b3788f2e583bd1134de755926da17a79ef2e8
Robert Tuck <robtuck@gmail.com>
2017-10-26 08:57:45 -0400

    Implemented PUT for /api/employees/:id

--- api/employees.js
+++ api/employees.js
@@ -63,3 +63,32 @@ employeesRouter.post('/', (req, res, next) => {
 employeesRouter.get('/:employeeId', (req, res, next) => {
   res.status(200).json({employee: req.employee});
 });
+
+employeesRouter.put('/:employeeId', (req, res, next) => {
+  const name = req.body.employee.name;
+  const position = req.body.employee.position;
+  const wage = req.body.employee.wage;
+  const isCurrentEmployee = req.body.employee.isCurrentEmployee === 0 ? 0 : 1;
+  if (!name || !position || !wage ) {
+    res.status(400).send();
+  } else {
+    const employeeSql = 'UPDATE Employee SET name = $name, position = $position, ' +
+                        'wage = $wage, is_current_employee = $isCurrentEmployee';
+    const employeeVals = {
+      $name: name,
+      $position: position,
+      $wage: wage,
+      $isCurrentEmployee: isCurrentEmployee
+    };
+    db.run(employeeSql, employeeVals, function(error) {
+      if (error) {
+        next(error);
+      } else {
+        db.get(`SELECT * FROM Employee WHERE Employee.id = ${req.params.employeeId}`,
+        (error, employee) => {
+          res.status(200).json({employee: employee});
+        });
+      }
+    });
+  }
+});
